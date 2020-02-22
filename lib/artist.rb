96d9be9877be6ef8e04e05b34a0c3c3d6bc959ec
poignant-writer-1163 <edmundopierre@yahoo.com>
2020-02-22 16:01:47 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -8,6 +8,13 @@ class Artist
     @@all << self
     @songs =[]
   end
+  
+  
+  
+  
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
 
   def add_song(song)
    # @songs << song
